d5c02ca1324e3ca3601b80786b0afc1eb733252d
halalsenpai <abdchaghtai@gmail.com>
2021-01-15 23:19:34 +0500

    new fork for hassan

--- src/components/StepForm/Time.js
+++ src/components/StepForm/Time.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
+import "./formstyles.css";
 
-export default function Time() {
-	const inputStyles = {
-		margin:'2px',
-		height:'70%'
-	
-	}
-	return (
-		<div style={{ display: "flex", justifyContent: "space-evenly", alignItems:'center' }}>
-			<input style={inputStyles} type="time" />
-			<input style={inputStyles} type="time" />
-			<Button style={inputStyles} primary >Select All</Button>
-		</div>
-	);
+export default function Time({ time, setForm }) {
+  // console.log(time.times.map((days) => console.log(days)));
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        flexGrow: "1",
+        margin: "1px 0",
+      }}
+    >
+      <input
+        name={time.times}
+        defaultValue="07:45"
+        onChange={setForm}
+        type="time"
+      />
+      <input name="mondayEndTime" type="time" />
+      <Button primary>Select All</Button>
+    </div>
+  );
 }
